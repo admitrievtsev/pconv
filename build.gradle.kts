5188b991b0489a49362b8d392d689c2dd3eba6f2
Aleksei <dmt.alex2005@gmail.com>
2025-04-14 00:20:49 +0300

    add: init simple java project
    Signed-off-by: Aleksei <dmt.alex2005@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("application")
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "core.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "pconv"
+
+include("src")
